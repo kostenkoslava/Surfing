4fa908749a1de52a357da672cff0ea58443fc1d9
Slava Kostenko <shegan782@gmail.com>
2020-10-12 00:02:41 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
+$(function () {
 
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    nextArrow: ' <img class="slider-arrows slider-arrows__right" src="img/aroows-right.svg" alt="right">',
+    prevArrow: ' <img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="right">',
+  })
 
+  $('.slider-dots').slick({
 
-  
-});
+  });
+})
